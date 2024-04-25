9e67e81569c99828916c3355d555b13a262878c0
Jenkins <jenkins@example.com>
2024-04-25 08:32:31 +0000

    updates

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,10 +1,8 @@
-const mongoose = require('mongoose');
 
-// MongoDB connection URI with username and password
-const uri = `mongodb://admin:admin123@mern-microservices-1-db:27017/microservices-test`;
+var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(uri, {
+mongoose.connect(process.env.MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
--- init-db.sh
+++ init-db.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# Wait for MongoDB to be ready
+until mongo admin --eval 'quit()' &>/dev/null; do
+    echo "Waiting for MongoDB to be ready..."
+    sleep 2
+done
+
 # Connect to MongoDB and create a new user
 mongo admin --eval 'db.createUser({ user: "admin", pwd: "admin123", roles: ["readWrite", "dbAdmin"] })'
